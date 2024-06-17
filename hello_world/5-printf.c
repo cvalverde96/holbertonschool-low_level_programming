24ad6ac33855e473bb5ff83ff86d2604bcbed275
cvalverde96 <cvalverde96@gmail.com>
2024-06-17 15:21:23 -0400

    printeando

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ *la funcion de este codigo
+ *es printear una oracion usando printf en int main
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
+
