fdc363517bcf97461156edc04301cf8f0b32167b
cvalverde96 <cvalverde96@gmail.com>
2024-06-17 15:36:57 -0400

    anadiendo comentarios.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - this function prints a sentence but it uses puts.
+ * Return: it will print 0 opon succesfull completion.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
