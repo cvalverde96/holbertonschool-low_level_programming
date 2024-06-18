4078260c6767d22d3b32c5845503387d073af284
cvalverde96 <cvalverde96@gmail.com>
2024-06-18 19:24:17 -0400

    imprimiendo del 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - imprimiendo todos los numeros usando while
+ * Return: 0 significa extito
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
+
+
