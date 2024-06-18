f5d3abf2d6e45d243c5bcb2f0cec1d7a57eb0a63
cvalverde96 <cvalverde96@gmail.com>
2024-06-18 15:39:16 -0400

    subiendo todos los files como ejecutablez junto con README.md

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - validando si numero random es numero positivo
+ *  negativo o zero.
+ *  Return: devuelve 0 si fue exitoso
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+"haciendo README file para 
+proyecto C - Variables, if, else, while"
