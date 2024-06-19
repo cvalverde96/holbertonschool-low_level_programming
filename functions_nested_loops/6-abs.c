c558aaab67617209f6e85b8a5f24e15cf894d3e2
cvalverde96 <cvalverde96@gmail.com>
2024-06-19 17:41:28 -0400

    subiendo numero 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - return absolute value
+ * @n: the value o main
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
