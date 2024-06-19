d5c4e704742c6525a9b8f954348ed825c3dba843
cvalverde96 <cvalverde96@gmail.com>
2024-06-19 16:00:05 -0400

    subiendo 3 ejercicio

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - va validar si variable c es mayor
+ * o igual a, y si c es igual o menor a zeta
+ * mientras tanto esto sea cierto programa correra
+ * sino devolvera 0
+ * Return: 0 significa completado
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
