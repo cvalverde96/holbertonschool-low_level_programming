76d2ae9947c66523722276b971928f97e562abe9
cvalverde96 <cvalverde96@gmail.com>
2024-06-19 20:08:50 -0400

    sumando dos numeros

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - imprime dos numeros int
+ * @n: primer valor de main
+ * @m: segundo valor de main
+ * Return: a valor de suma
+ */
+
+int add(int n, int m)
+{
+	int a = n + m;
+
+	return (a);
+
+}
+
