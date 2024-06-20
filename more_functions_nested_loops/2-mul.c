bc1d49198f1784ad6ed8c0991608e19c007e681d
cvalverde96 <cvalverde96@gmail.com>
2024-06-20 10:19:15 -0400

    funcion que multiplica dos numeros

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplica dos numeros
+ * @a: primera variable
+ * @b: segunda variable
+ * Return: 0 es exito
+ */
+
+int mul(int a, int b)
+{
+	int M = a * b;
+
+	return (M);
+}
