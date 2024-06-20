1e0f48fe776e9476d4cd6d3f7e7b94a8573e470b
cvalverde96 <cvalverde96@gmail.com>
2024-06-20 16:53:54 -0400

    Lines go burrrrr

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,29 @@ void more_numbers(void)
 		n = 0;
 		while (n <= 14)
 		{
+			/* si n es MAYOR a 9
+			 * vas a poner el caracter 1 en pantalla
+			 * y luego ejectuas el segundo putchar
+			 */
 			if (n > 9)
+			{
 				_putchar('1');
+			}
+			/*
+			 * este putchar va correr indiferentemente 
+			 * de si if se cumple o no ya que no es un else
+			 * esto va tomar el valor de n y lo va modulos 10
+			 * esta operacion SIEMPRE devuelve el ultimo digito
+			 * como un un numero con un solo digito  siempre
+			 * devuelve el mismo digito no importa,
+			 * pero cuando es un numero con dos digitos, 
+			 * vamos a devolver el ultimo digito para que 
+			 * putchar lo pueda imprimir
+			 * no obrante esto lo va devolver como un INT
+			 * por lo cual lo tenemos que sumar con un CHAR
+			 * que en este caso seria '0' y lo convierte en 
+			 * su equivalente char atraves de ASCII
+			 */
 			_putchar((n % 10) + '0');
 			n++;
 		}
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - imprime lineas
+ * @n: sdas
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
