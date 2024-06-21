e2140874660fad83a97570d128a049ee9ee96390
cvalverde96 <cvalverde96@gmail.com>
2024-06-20 21:09:05 -0400

    pushing 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,16 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 
 		else
@@ -30,10 +30,6 @@ int main(void)
 			printf("%d ", i);
 		}
 
-		if (i < 100)
-		{
-			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
