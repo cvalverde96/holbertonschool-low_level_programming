84ed1caac2f3b0a9e633a72b56e858b08b1c31e6
cvalverde96 <cvalverde96@gmail.com>
2024-06-24 19:28:09 -0400

    subiendo cuart

---
+++ pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - reverse string
+ * Return: 0 es exito
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
