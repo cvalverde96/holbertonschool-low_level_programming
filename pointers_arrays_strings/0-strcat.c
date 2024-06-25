279e679d28d457e665f2da5b9ab81b7e673e8238
cvalverde96 <cvalverde96@gmail.com>
2024-06-25 15:26:28 -0400

    arreglando el zero

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -18,7 +18,6 @@ char *_strcat(char *dest, char *src)
 	{
 		lenDest++;
 	}
-	_putchar(lenDest);
 
 	while (src[lenSrc] != '\0')
 	{
---
+++ pointers_arrays_strings/test.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatanets two strings
+ * @dest: pointer
+ * @src: pointer
+ * Return: dest va devlver el cpointer al string concatanetated
+ */
+
+char *_strcat(char *dest, char *src)
+{
+        int lenDest, lenSrc;
+
+        lenDest = 0;
+        lenSrc = 0;
+
+        while (dest[lenDest] != '\0')
+        {
+                lenDest++;
+        }
+        _putchar(lenDest);
+}
