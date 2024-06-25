d8b0e3e55f57658c76505451a61d8623d35c3682
cvalverde96 <cvalverde96@gmail.com>
2024-06-24 21:46:21 -0400

    subiendo novena

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copiando string
+ * @dest: pointer var
+ * @src: pointer var
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
