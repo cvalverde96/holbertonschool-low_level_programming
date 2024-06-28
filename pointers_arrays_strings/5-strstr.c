1547437435e3198d1e4c8784288f54c5f65a4c14
cvalverde96 <cvalverde96@gmail.com>
2024-06-27 22:09:25 -0400

    subiendo la quinta

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _memcmp - funcion
+ * @s1: pointer
+ * @s2: pointer
+ * @n: integer
+ * Return: 0
+ */
+
+int _memcmp(const void *s1, const void *s2, int n);
+
+/**
+ * _strstr - function
+ * @haystack: pointer
+ * @needle: p
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int n = _strlen(needle);
+
+	while (*haystack)
+		if (!_memcmp(haystack++, needle, n))
+			return (haystack - 1);
+	return (0);
+}
+
+/**
+ * _strlen - coge una variable y la imprime una por una
+ * @s: pointer de string
+ * Return: 0 es exito
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _memcmp - funcion
+ * @s1: pointer
+ * @s2: pointer
+ * @n: variable
+ * Return: return 0
+ */
+
+int _memcmp(const void *s1, const void *s2, int n)
+{
+	const unsigned char *p1 = s1, *p2 = s2;
+
+	while (n--)
+		if (*p1 != *p2)
+			return (*p1 - *p2);
+		else
+			p1++, p2++;
+	return (0);
+}
+
+
