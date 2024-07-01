f018a7e415829c749aa331adb11f274bdd343b68
cvalverde96 <cvalverde96@gmail.com>
2024-07-01 13:51:27 -0400

    subiendo la dos

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - cogiendo lenght of stirng
+ * @s: pointer to string
+ * Return: value of lenght
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return (0);
+	}
+
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+
+}
