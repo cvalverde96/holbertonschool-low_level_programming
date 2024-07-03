f724279a4f7aa2459fc6418c9b61cb5c5c5a2720
cvalverde96 <cvalverde96@gmail.com>
2024-07-03 11:57:10 -0400

    subiendo 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creando un array
+ * @size: tamano en bytes
+ * @c: caracter
+ * Return: arr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	unsigned int i;
+
+	arr = malloc(sizeof(*arr) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+
+	}
+
+	return (arr);
+
+
+}
