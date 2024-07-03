4e1e51feffed53fab9a46de0da81e8e1bf5bc7bf
cvalverde96 <cvalverde96@gmail.com>
2024-07-03 15:12:54 -0400

    subiendo la 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function que va validar si string esta NULL
+ * luego con while statement va calcular LENGHT de str
+ * luego le va asignar ese valor en bytes a dupli
+ * a traves de malloc + 1 para null terminator
+ * for va ir asignando valor de str a dupli
+ * @str: string de main
+ * Return: dupli
+ */
+
+char *_strdup(char *str)
+{
+	char *dupli;
+	int length = 0;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	dupli = malloc(length + 1);
+
+	for (i = 0; i < length; i++)
+	{
+		dupli[i] = str[i];
+	}
+
+	return (dupli);
+}
