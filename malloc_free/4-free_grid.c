afe171dc94fafd36c58363155fd1d95cff13e879
cvalverde96 <cvalverde96@gmail.com>
2024-07-04 11:19:52 -0400

    subiendo la cuatro

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - funcion para liberar grid
+ * @grid: pointer to pointer
+ * @height: alto
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i;
