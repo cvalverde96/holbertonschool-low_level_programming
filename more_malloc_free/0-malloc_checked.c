d02d6c812fc9066244457474426710802516ea57
cvalverde96 <cvalverde96@gmail.com>
2024-07-04 11:35:33 -0400

    subiendo 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - asignando memoria usando malloc
+ * @b: cantidad de bytes
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(sizeof(*ptr) * b);
+
+	return (ptr);
+}
