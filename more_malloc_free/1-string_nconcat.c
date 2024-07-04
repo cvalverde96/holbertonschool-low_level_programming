65cf03fd18a1473086eb0b439b7400b99a901d97
cvalverde96 <cvalverde96@gmail.com>
2024-07-04 12:50:41 -0400

    subiendo la 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len_s1, len_s2, i;
 	char *str;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	len_s1 = _strlen(s1);
 	len_s2 = _strlen(s2);
 
