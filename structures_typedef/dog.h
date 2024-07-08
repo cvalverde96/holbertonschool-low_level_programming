a671f57210c352fb369e2bf33b9bfc9b4332c039
cvalverde96 <cvalverde96@gmail.com>
2024-07-08 11:16:13 -0400

    subiendo dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - estamos creando una estructura dogo
+ * @name: pointer to char para string de nombre de dogo
+ * @age: float para age
+ * @owner: pointer to char para string de nombre de owner de dogo
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
+
+#endif
