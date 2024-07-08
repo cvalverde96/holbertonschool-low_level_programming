3397949ddc8b7471b6f324b2426a4ebcd9d2643f
cvalverde96 <cvalverde96@gmail.com>
2024-07-08 12:10:56 -0400

    subiendo la 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+ * init_dog - initializing dog strucutre
+ * @d: struct dog *d
+ * @name: pointer to char para string de nombre de dogo
+ * @age: edad de dogo en float
+ * @owner: pointer to char para string de nombre de owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d = malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
