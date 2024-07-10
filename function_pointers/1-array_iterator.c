8dc02b9fb60b8affd51c21e1e7ff79978b3c42f6
cvalverde96 <cvalverde96@gmail.com>
2024-07-10 12:54:49 -0400

    subiendo la 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function that executes a function given as a parameter on each element of an array
+ * array_iterator - function that executes a
+ * function given as a parameter on each element of an array
  * @array: pointer to int
  * @size: tamano
  * @action: pointer to function
