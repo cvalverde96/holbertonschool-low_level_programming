ff305bbf7e3162b150cb53f67e9b7b5651d7d4f8
cvalverde96 <cvalverde96@gmail.com>
2024-07-12 16:29:53 -0400

    subiendo op

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - operation of addition
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - operation of substraction
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - opertation of multiplication
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - operation of division
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - operation of modulus
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
