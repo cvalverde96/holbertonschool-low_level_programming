3d615110e8996e310046c3741e7a6af632c498c1
cvalverde96 <cvalverde96@gmail.com>
2024-07-12 16:29:41 -0400

    subiendo get op

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - function to pointer
+ * @s: string
+ * Return: return
+ */
+
+
+int (*get_op_func(char *s))(int, int)
+{
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+
+	while (*ops[i].op != '\0')
+	{
+		i++;
+	}
+
+	ops[i].f;
+
+}
